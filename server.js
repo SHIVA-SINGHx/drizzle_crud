6e18040a1747140aeb688540e706fd80f5f64d30
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2026-01-17 21:32:39 +0530

    Fix the update and delete query

--- server.js
+++ server.js
@@ -5,48 +5,44 @@ import { usersTable } from "./src/drizzle/schema.js";
 
 const main = async ()=>{
 
-    // insert a user (Create)
-    const insertUser = await db.insert(usersTable).values({
-        name: "Shiva Singh",
-        age: 18,
-        email: "shiva.singh@example.com", 
-    })
-    console.log("Inserted User:", insertUser);
+    //1. Insert a user (Create)
+    // const insertUser = await db.insert(usersTable).values({
+    //     name: "Shiva Singh",
+    //     age: 18,
+    //     email: "shiva.singh@example.com", 
+    // })
+    // console.log("Inserted User:", insertUser);
 
 
-    /// 2.insert multiple users (Create)
-        const insertUser2 = await db.insert(usersTable).values([
-        {
-            name: "John Doe",
-            age: 25,
-            email: "john.doe@example.com"
-        },
-        {
-            name: "Jane Smith",
-            age: 30,
-            email: "jane.smith@example.com"
-        }
-    ])
+    /// 2.Insert multiple users (Create)
+    //     const insertUser2 = await db.insert(usersTable).values([
+    //     {
+    //         name: "John Doe",
+    //         age: 25,
+    //         email: "john.doe@example.com"
+    //     },
+    //     {
+    //         name: "Jane Smith",
+    //         age: 30,
+    //         email: "jane.smith@example.com"
+    //     }
+    // ])
 
     // 3. fetch all users (READ)
-    
     // const getUser = await db.select().from(usersTable);
     // console.log("All users: ", getUser)
 
-    // 4. Update a user (UPDATE)
 
+    // 4. Update a user (UPDATE)
     const updateUser = await db.update(usersTable)
     .set({name: "Shiva Updated"})
-    .where(eq(usersTable.email, "shiva.singh@example.com"))
+    .where(eq(usersTable.email, "shiva@example@gmail.com"))
 
     console.log("Updated User: ", updateUser);
 
     // 5. Delete a user (DELETE)
-
     await db.delete(usersTable).where(eq(usersTable.email, "shiva.singh@example.com"));
 }
-
-
 
 
 // const app = epxress();
